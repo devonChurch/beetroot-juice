f3732247434f248cd3986fd2f486b00e7fd2b53d
strivegfx <notifydevon@gmail.com>
2016-03-09 21:44:18 +1300

    Am now curating some real podcast data from Xero

--- src/js/episodes.js
+++ src/js/episodes.js
@@ -67,8 +67,8 @@ module.exports = React.createClass({
 								<a className="episode__link" href="#" onClick={this.activateEpisode.bind(this, current)}>
 									<Svg component={'episode'} icon={'item'}/>
 									<h2 className="episode__title">{episode.title}</h2>
-									<span className="episode__date">12 February 2016</span>
-									<p className="episode__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+									<span className="episode__date">{episode.date}</span>
+									<p className="episode__description">{episode.desc}</p>
 								</a>
 								{this.state.active === current ? <Player json={this.props.json[current]} cursor={this.state.cursor}/> : ''}
 							</li>
--- src/js/feed.js
+++ src/js/feed.js
@@ -2,53 +2,93 @@
 
 module.exports = [
 	{
-		title: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+		title: 'A guide to handling difficult employees',
+		date: 'Wed, 24 Feb 2016 08:00:00 +0000',
+		desc: 'Save yourself time, energy and money by hiring people who fit into the culture of your small business rather than hiring based on skillset alone. This is the key message from Nancy Riegle, VP Human Resources at the National Association of Manufacturers in the latest Xero Gravity episode. And Nancy ought to know. With HR experience spanning more than 20 years, Nancy has managed it all from employees that bicker like children to a disgruntled machete-wielding worker. Nancy speaks with hosts Gene Marks and Elizabeth U about how to best handle difficult employees in small business while sharing some insightful life lessons about why life is too short to not love what you do.',
+		mp3: 'http://traffic.libsyn.com/xerohour/Xero_Gravity_Podcast_-_Nancy_Reagle_1.1.mp3'
+	},
+	{
+		title: 'The spirit of having entrepreneurial mentors',
+		date: 'Wed, 17 Feb 2016 08:00:00 +0000',
+		desc: 'Save yourself time, energy and money by hiring people who fit into the culture of your small business rather than hiring based on skillset alone. This is the key message from Nancy Riegle, VP Human Resources at the National Association of Manufacturers in the latest Xero Gravity episode. And Nancy ought to know. With HR experience spanning more than 20 years, Nancy has managed it all from employees that bicker like children to a disgruntled machete-wielding worker. Nancy speaks with Xero Gravity hosts Gene Marks and Elizabeth U about how to best handle difficult employees in small business while sharing some insightful life lessons about why life is too short to not love what you do.',
 		mp3: 'http://traffic.libsyn.com/xerohour/Xero_Gravity_Podcast_-_Courtney_and_JD_1.1.mp3'
-
 	},
 	{
-		title: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+		title: 'Succession planning: Is it your happily ever after?',
+		date: 'Wed, 10 Feb 2016 08:00:00 +0000',
+		desc: 'Are you looking to start your next business, or maybe thinking about retirement? Succession planning is the key to making sure your small business journey ends with a ‘happily ever after’. In this episode of Xero Gravity, Dr. Sabine Rau, Professor of Entrepreneurship and Family Business at King’s College London, and Judy Wicks, author and entrepreneur, drop by to share their stories about succession planning with Gene and Elizabeth. Together, Sabine and Judy illuminate the difference between ownership and leadership, discuss issues of family inheritance and show how your key values can be hardwired into a business before sale. Sabine urges small business owners to be very clear about what they’re trying to achieve when looking at succession. “You can\'t have it all,” she said. “You have to be very clear what you want to achieve.” We find out how you can do it with grace and success.',
 		mp3: 'http://traffic.libsyn.com/xerohour/Xero_Gravity_Podcast_-_Judy_and_Dr_Sabine_1.1.mp3'
-
 	},
 	{
-		title: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+		title: 'You\'re never too young to plan for retirement',
+		date: 'Wed, 03 Feb 2016 08:00:00 +0000',
+		desc: 'You could be sipping martinis on a tropical island sooner than you thought. It all comes down to implementing an effective retirement plan. In this episode of Xero Gravity, Rochelle Odessor, vice president, Madison Planning Group and Justin Follmer, MBA at Coastal Wealth Advisors catch up with Gene and Elizabeth to dive into the details of retirement planning. Approximately half of all small businesses in the States with one or two employees have a retirement plan in place, leaving the other half with their planning yet to come. With innovative new legislation and an arsenal of instruments to ensure small business owners are making the right types of investments, today is a great day to get inspired to put your future finances on the right path.',
 		mp3: 'http://traffic.libsyn.com/xerohour/Xero_Gravity_Podcast_-_Rochelle_and_Justin_2.0.mp3'
-
 	},
 	{
-		title: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+		title: 'Threats to small business: natural and cyber',
+		date: 'Wed, 27 Jan 2016 08:00:00 +0000',
+		desc: 'You can’t put a price on peace of mind — especially with the real threat to small businesses from natural and cyber security. So what precautions can you take to safeguard against a cyber attack or natural disaster? In Xero Gravity #32, David Raissipour, SVP of Product & Engineering at Carbonite, and Russ Findlay Head of Marketing for Hiscox USA, join us to debunk all that’s unsettling about security, data breaches, data backups and disaster insurance. We’ll share how you can allay your fears and remove the dangers, by taking the proper measures, simply and affordably. You’ll learn how mitigating these risks to your data and business can increase profits and peace of mind. Listen in, and never look back!',
 		mp3: 'http://traffic.libsyn.com/xerohour/Xero_Gravity_Podcast_-_David_and_Russ_2.0.mp3'
-
 	},
 	{
-		title: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+		title: 'How to keep your staff happy motivated',
+		date: 'Wed, 20 Jan 2016 08:00:00 +0000',
+		desc: 'An appreciated employee is your best asset. So attracting and keeping staff must extend far beyond good pay, benefits and free Friday lunches. Workers are on the hunt for real perks, flexible schedules and a salary package that will keep their job satisfaction level high. Above all that means being heard, respected and trusted. Intangibles that say, “I’m appreciated.” How will you stand apart in today’s highly competitive marketplace, where businesses one-up each other for new talent? Can you find employees who will enjoy their work, stay longer than a year or two, and become advocates for your business? In this episode of Xero Gravity, Comedy CPE founder, Greg Kyte and Josh Reeves, CEO at Gusto, debunk the myths of what makes employees tick, and share ways you can boost your business culture to ensure they stay motivated and happy.',
 		mp3: 'http://traffic.libsyn.com/xerohour/Xero_Gravity_Podcast_-_Josh_and_Greg_3.0.mp3'
-
 	},
 	{
-		title: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
-		mp3: 'http://traffic.libsyn.com/xerohour/Xero_Gravity_Podcast_-_Courtney_and_JD_1.1.mp3'
-
+		title: 'Accessing Capital for your Business: thoughts from TD Bank Xero executives',
+		date: 'Wed, 13 Jan 2016 08:00:00 +0000',
+		desc: 'On episode 30, we’ll share insights on how you can bring your business idea(s) to fruition through access to capital funding. As a business owner / entrepreneur, you’re probably aware of the traditional lending options out there. Yet there are many other ones available. What we’re going to do is help remove the tricky parts from the equation, so you can find those that best suit you and your business model. Together with our guests, President of Xero, US, Russell Fujioka, and of Small Business, Restaurant & Government Banking, at TD Bank, Jay DesMarteau, we’ll talk about the multitude of ways you can get the equity you need confidently, now, along with what the future of lending looks like.',
+		mp3: 'http://traffic.libsyn.com/xerohour/Xero_Gravity_Podcast_-_Russ_and_Jay_2.0.mp3'
 	},
 	{
-		title: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
-		mp3: 'http://traffic.libsyn.com/xerohour/Xero_Gravity_Podcast_-_Judy_and_Dr_Sabine_1.1.mp3'
-
+		title: 'Turning your business passion into a company',
+		date: 'Wed, 06 Jan 2016 08:00:00 +0000',
+		desc: 'Taking your passion from a hobby to full-time business can be a challenge. But not an impossible one. On this week’s Xero Gravity podcast episode, Emma Chapman, co-founder of online business A Beautiful Mess, drops by to chat with Gene and Elizabeth about this very subject. Between her tips on the ways to look after finances and your three-year forecast, to how to hire your first employee and balancing work with your personal life, Emma’s insightful ideas — inspired by her own journey — are sure to enhance your entrepreneurial skillset. So you can make your dream business a reality, too. As a bonus, Emma’s offering all Xero Gravity listeners $15 off A Beautiful Mess’ new small business course: Start Small, Dream Big. To redeem this limited time offer, simply head to shop.abeautifulmess.com and enter the promo code DREAMBIG at checkout.',
+		mp3: 'http://traffic.libsyn.com/xerohour/Xero_Gravity_Podcast_-_Emma_Chapman_2.0.mp3'
 	},
 	{
-		title: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
-		mp3: 'http://traffic.libsyn.com/xerohour/Xero_Gravity_Podcast_-_Rochelle_and_Justin_2.0.mp3'
-
+		title: 'Tech disruption: That mobile madness',
+		date: 'Wed, 30 Dec 2015 08:00:00 +0000',
+		desc: 'Technology continues to disrupt the flow of business, making it more important than ever to understand the powerful role apps can play. How can you combine mobile and apps in your business? How are they being used across the world? What would a planet without them look like? In episode 28 of Xero Gravity, hosts Gene Marks and Elizabeth explore mobile applications and the opportunities they present for small business. Joined by Mark Paddon of Guide Kick, and Redmer Schukken of Ice House Corporation, Gene and Elizabeth ask some pointed questions about app development and use. And delve into there connection to online security and the streamlining of business operations. All that and more — including why Southeast Asian businesses are bypassing desktops and going straight to mobile — from the delightful duo of hosts Elizabeth Ü and Gene Marks!',
+		mp3: 'http://traffic.libsyn.com/xerohour/Xero_Gravity_Podcast_-_Redmer_and_Mark_2.0.mp3'
 	},
 	{
-		title: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
-		mp3: 'http://traffic.libsyn.com/xerohour/Xero_Gravity_Podcast_-_David_and_Russ_2.0.mp3'
-
+		title: '10 Smart New Year’s resolutions for small business and how to keep them!',
+		date: 'Wed, 23 Dec 2015 08:00:00 +0000',
+		desc: 'Chances are the line between your professional and personal life sometimes gets blurred. 2016 is a chance to set boundaries, once and for all. Here’s 10 ways you can create the life you envisioned when you started your business, or maintain the balance you already have. In episode 27 of Xero Gravity, we chat with Michelle Garcia, founder of Heirloom Catering, and Jeremy Smith, who heads up the Block Small Business division at H&R Block, about their business resolutions for 2016. Michelle explains how her company, finally out of “survival” mode, will implement a complete “no more excuses” overhaul of her bookkeeping method. While on the flipside, Jeremy’s advice to business owners will be to start 2016 with a detailed 12-month plan. All that and more, from the delightful duo of hosts Elizabeth Ü and Gene Marks!',
+		mp3: 'http://traffic.libsyn.com/xerohour/Xero_Gravity_Podcast_-_Michelle_and_Jeremy_1.0.mp3'
 	},
 	{
-		title: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
-		mp3: 'http://traffic.libsyn.com/xerohour/Xero_Gravity_Podcast_-_Josh_and_Greg_3.0.mp3'
-
+		title: 'How to partner with big box retailers: Is it a good idea?',
+		date: 'Wed, 16 Dec 2015 08:00:00 +0000',
+		desc: 'When it comes to partnering with big box retailers, there are many myths for small business owners to be aware of. So how exactly do go about setting up a partnership? What do big box retailers expect from small businesses? And, is it a good idea? In this episode of Xero Gravity, hosts Gene Marks and Elizabeth U chat to Laura Curtis, founder of Malvi Marshmallow Confections, and Howard Saunders, retail futurist with 22nd and 5th Limited, about making these partnerships work. Laura shares insights from her partnerships with outlets such as Dean & DeLuca’s and Anthropologie. Howard also unpacks some of the mistakes he’s witnessed during his 25 years in the retail industry. “Big Retailers aren’t going to make a fortune out of you,” Howard says. “You will make a profit, but the reason they need small business is to make them look locally connected. It makes them look more artisan and this gives them kudos.”',
+		mp3: 'http://traffic.libsyn.com/xerohour/Xero_Gravity_Podcast_-__Laura_and_Howard_2.0.mp3'
+	},
+	{
+		title: 'Great ways to increase your cash flow in 2016',
+		date: 'Wed, 09 Dec 2015 08:00:00 +0000',
+		desc: 'This episode of Xero Gravity is all about cash flow; the lifeblood of any small business. How do you improve cash flow? How do you generate an income and a livelihood? And how do you stay tough during the hard times? Will Slade, founder of accounting and bookkeeping firm Slade and Company, drops by to chat to hosts Gene Marks and Elizabeth U about navigating cash flow management and effective budgeting to stay afloat. “If you have situations where you’re not getting the consistent income that everyone would love to have in business, you have to make sure you have back up resources and an understanding of the trends in the business that will allow you to get through the bad times in preparation for the good times,” Will tells Gene and Elizabeth.',
+		mp3: 'http://traffic.libsyn.com/xerohour/Xero_Gravity_Podcast_-_Will_Slade_2.0.mp3'
+	},
+	{
+		title: 'Is crowdfunding your magic revenue wand?',
+		date: 'Wed, 02 Dec 2015 08:00:00 +0000',
+		desc: 'The opportunities crowdfunding presents for small business owners and entrepreneurs are almost endless - but how far can it really get you? And what are the traps you need to be aware of? In this episode of Xero Gravity, hosts Gene Marks and Elizabeth U chat to Lenore Estrada, co-founder at Three Babes Bakeshop, about how she made crowdfunding work for the launch of her business. Lenore shares some of the ups and downs that come from launching via a crowdfunding model, including just how much work is really involved to make it work for you. “Launching the Kickstarter campaign is really what launched the press explosion that happened when our business started,” Lenore told Gene and Elizabeth. “We just weren’t as thoughtful as we should have been logistically about how we were going to deliver on some of the things we were offering.”',
+		mp3: 'http://traffic.libsyn.com/xerohour/Xero_Gravity_Podcast_-_Lenore_Estrada_1.0.mp3'
+	},
+	{
+		title: 'Holiday cheer: revenue secrets from service businesses',
+		date: 'Wed, 25 Nov 2015 08:00:00 +0000',
+		desc: 'This week on Xero Gravity we shine a spotlight on how the holiday season impacts service businesses and what small business owners in this industry can do with their time to capitalise on the holiday rush. Hosts Gene and Elizabeth are joined by Alexander Gray, President & CEO at Skylight Global, and Tyler Jensen, CEO at The Startup Garage, to chat about taking advantage of the holiday season, marketing at this time of year and even taking a well-deserved break. According to Alexander, success for service businesses during this season is driven by authenticity and effective storytelling. "Your customers want to know that whoever is emailing them actually has something authentic to share with them. So mastering the art of storytelling, especially around the Christmas holidays is essential," he says.',
+		mp3: 'http://traffic.libsyn.com/xerohour/Xero_Gravity_Podcast_-_Alex_and_Tyler_1.0.mp3'
+	},
+	{
+		title: 'The Holidays are Coming and the Goose is Getting Fat',
+		date: 'Wed, 18 Nov 2015 08:00:00 +0000',
+		desc: 'Are you business-ready for the holiday season? Have you thought about what comes after December? In this episode, Gene and Elizabeth share tips to help you prepare for the busiest retail season of the year. They also chat to Kelly Barker, founder and chief empowerment office at Prep Cosmetics, and Clint Bowers, founder and CEO of Smart Business Concepts, about how they’re preparing for the sales rush and what they would recommend to those who are new to the Christmas sales market. According to Gene, “people tend to get all wrapped up with the holiday season and make mistakes as a result.” These mistakes can range from staffing mismanagement to choosing the wrong technologies to not planning ahead. “These are all the kinds of things we want to make sure that you avoid when planning for this holiday season,” He says. ',
+		mp3: 'http://traffic.libsyn.com/xerohour/Xero_Gravity_-_Ep_1_2.0.mp3'
 	}
 ];
